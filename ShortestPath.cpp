195f30df34b479b8e3481101767abae33cd030d6
cvrac <cvracdi@gmail.com>
2016-11-12 00:28:04 +0200

    medium 3.58 secs :)

--- ShortestPath.cpp
+++ ShortestPath.cpp
@@ -31,65 +31,62 @@ int ShortestPath::shortestPath(uint32_t& source, uint32_t& target) {
 	explored_set_.insert(source);
 	explored_set_x.insert(target);
 
-	uint32_t c1 = 0, c2 = 0;
+	uint32_t c1 = pr_graph_.getNeighborsCount(source, dirf_), c2 = pr_graph_.getNeighborsCount(target, dirb_);
 	while (true) {
 
 		//no solution, return failure
 		if (frontier_front_.empty() || frontier_back_.empty())
 			return -1;
 
-		if (clevelf_ <= clevelb_) {
-			// case 1:
-			node_id = frontier_front_.pop();
-			--clevelf_;
-			c1 = 0; c2 = 0;
+		if (clevelf_ + c1 <= clevelb_ + c2) {
 
-			//expand node
-			NodeArray &neighbors = pr_graph_.getNeighbors(node_id, dirf_);
-			for (int i = 0; i < neighbors.count; i++) {
-				tempId = neighbors.array[i];
-				if (explored_set_x.search(tempId))  {
-					return distance_front_ + distance_back_ + 1;
-				} else if (explored_set_.searchInsert(tempId)) {
-					//explored_set_.insert(tempId);
-					frontier_front_.push(tempId);
-					++clevelf1_;
-					c1 += pr_graph_.getNeighborsCount(tempId, dirf_);
+			c1 = 0;
+			while (clevelf_ > 0) {
+				node_id = frontier_front_.pop();
+				--clevelf_;
+				// c1++;
+
+				//expand node
+				NodeArray &neighbors = pr_graph_.getNeighbors(node_id, dirf_);
+				for (int i = 0; i < neighbors.count; i++) {
+					tempId = neighbors.array[i];
+					if (explored_set_x.search(tempId))  {
+						return distance_front_ + distance_back_ + 1;
+					} else if (explored_set_.searchInsert(tempId)) {
+						//explored_set_.insert(tempId);
+						frontier_front_.push(tempId);
+						++clevelf1_;
+						c1 += pr_graph_.getNeighborsCount(tempId, dirf_);
+					}
 				}
 			}
+		 	++distance_front_;
+		 	clevelf_ = clevelf1_;
+		 	clevelf1_ = 0;
+		} else if (clevelf_ + c1 > clevelb_ + c2){
 
-			if (clevelf_ == 0) {
-				++distance_front_;
-				clevelf_ = clevelf1_ + c1;
-				clevelf1_ = 0;
-			}
-			// break;
-			// case 0:
-		} else if (clevelf_ > clevelb_){
-			node_id = frontier_back_.pop();
-			--clevelb_;
-
-			//expand node
-			NodeArray &neighbors = pr_graph_.getNeighbors(node_id, dirb_);
-			c2 = 0; c1 = 0;
-			for (int i = 0; i < neighbors.count; i++) {
-				tempId = neighbors.array[i];
-				if (explored_set_.search(tempId))  {
-					return distance_front_ + distance_back_ + 1;
-				} else if (explored_set_x.searchInsert(tempId)) {
-					//explored_set_x.insert(tempId);
-					frontier_back_.push(tempId);
-					++clevelb1_;
-					c2 += pr_graph_.getNeighborsCount(tempId, dirb_);
+			c2 = 0;
+			while (clevelb_ > 0) {
+				node_id = frontier_back_.pop();
+				--clevelb_;
+				// c2++;
+				//expand node
+				NodeArray &neighbors = pr_graph_.getNeighbors(node_id, dirb_);
+				for (int i = 0; i < neighbors.count; i++) {
+					tempId = neighbors.array[i];
+					if (explored_set_.search(tempId))  {
+						return distance_front_ + distance_back_ + 1;
+					} else if (explored_set_x.searchInsert(tempId)) {
+						//explored_set_x.insert(tempId);
+						frontier_back_.push(tempId);
+						++clevelb1_;
+						c2 += pr_graph_.getNeighborsCount(tempId, dirb_);
+					}
 				}
 			}
-
-			if (clevelb_ == 0) {
-				++distance_back_;
-				clevelb_ = clevelb1_ + c2;
-				clevelb1_ = 0;
-			}
-			// break;
+			++distance_back_;
+			clevelb_ = clevelb1_;
+			clevelb1_ = 0;
 		}
 	}
 
